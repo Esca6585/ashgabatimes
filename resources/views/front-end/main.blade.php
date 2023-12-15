6b1aa2dfe7c72097f9c75ac63b46d0d2fed79667
Esen Meredow <esca656585@gmail.com>
2023-12-15 11:53:55 +0500

    pushed

---
+++ resources/views/front-end/main.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.news-template')
+
+@section('title')
+    {{ config('app.name') }} {{ __('News') }}
+@endsection
+
+@section('item-header')
+    @include('front-end.item-header')
+@endsection
+
+@section('container-content')
+    @include('front-end.container-content')
+@endsection
+
+@section('footer')
+    @include('front-end.footer')
+@endsection
+
